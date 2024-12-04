1095da9bfbadeaa21318e47a63b9b3111fd3bad3
Uri <uri555459@gmail.com>
2024-12-04 21:53:17 +0300

    Added base config project

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import puppeteer from 'puppeteer'
+
+const link = 'https://freebitco.in'
+
+const start = async () => {
+	// Launch the browser and open a new blank page
+	const browser = await puppeteer.launch()
+	const page = await browser.newPage()
+
+	// Navigate the page to a URL.
+	await page.goto(link, { waitUntil: 'domcontentloaded' })
+
+	// Set screen size.
+	await page.setViewport({ width: 1080, height: 1024 })
+
+	// Type into search box.
+	// await page.locator('.devsite-search-field').fill('automate beyond recorder')
+}
+
+start()
