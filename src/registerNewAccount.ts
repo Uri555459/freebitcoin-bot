e81dd7fdc25ebaa54c18b6f61cb4db104f75d566
Uri <uri555459@gmail.com>
2024-12-04 23:17:59 +0300

    Added features get free email and add to array emailData.json

--- src/registerNewAccount.ts
+++ src/registerNewAccount.ts
@@ -1,20 +1,81 @@
+import fs from 'fs'
 import puppeteer from 'puppeteer'
 
-const link = 'https://tempmailto.org'
+const linkEmail = 'https://tempmailto.org'
+const linkBitcoin = 'https://freebitco.in'
+const fileDataPath = 'emailData.json'
 
+// Функция запуска
 const start = async () => {
-	// Launch the browser and open a new blank page
-	const browser = await puppeteer.launch()
-	const page = await browser.newPage()
+	// Настраиваем браузер
+	let browser = await puppeteer.launch({
+		headless: false,
+		slowMo: 100,
+		devtools: false,
+	})
+	let page = await browser.newPage()
 
-	// Navigate the page to a URL.
-	await page.goto(link, { waitUntil: 'domcontentloaded' })
+	// Переходим на страницу и ждем ее полной загрузки
+	await page.goto(linkEmail, { waitUntil: 'domcontentloaded' })
 
-	// Set screen size.
+	// Устанавливаем размер окна
 	await page.setViewport({ width: 1080, height: 1024 })
+
+	// Забираем email
+	const email = await page.$eval('#email_id', elem => elem.textContent)
+
+	// Если не нашли email, то выходим
+	if (!email) {
+		console.error('Email not found')
+		browser.close()
+		return
+	}
+
+	// Если файла нет, то создаем
+	if (!fs.existsSync(fileDataPath)) {
+		fs.writeFileSync(fileDataPath, JSON.stringify([email]), 'utf8')
+	}
+
+	// Читаем файл
+	const data = JSON.parse(fs.readFileSync(fileDataPath, 'utf8'))
+	// Добавляем в массив
+	data.push(email)
+	// Записываем обратно в файл
+	fs.writeFileSync(fileDataPath, JSON.stringify(data), 'utf8')
+
+	// Закрываем браузер
+	browser.close()
+
+	// Настраиваем новый браузер
+	browser = await puppeteer.launch({
+		headless: false,
+		slowMo: 100,
+		devtools: true,
+	})
+	page = await browser.newPage()
+	// Переходим на страницу и ждем ее полной загрузки
+	await page.goto(linkBitcoin, { waitUntil: 'domcontentloaded' })
+
+	// Вводим email
+	await page.type('#signup_form_email', email)
+
+	// Вводим password
+	await page.type('#signup_form_password', email)
+
+	// Подтверждаем что не робот
+	await page.mouse.click(434, 390)
+	await page.mouse.click(434, 390)
+
+	// Кликаем зарегистрироваться
+	// await page.click('#signup_button')
+	// Закрываем браузер
+	// browser.close()
 
 	// Type into search box.
 	// await page.locator('.devsite-search-field').fill('automate beyond recorder')
+
+	// top:354.0625 left: 536
 }
 
+// Запускаем
 start()
---
+++ src/test.js
@@ -0,0 +1,6 @@
+document.addEventListener('click', e => {
+	const x = e.clientX
+	const y = e.clientY
+
+	console.log(x, y)
+})
